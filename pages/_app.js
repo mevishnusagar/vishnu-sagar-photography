efd3dc9fa4e375dfb6c104dee0989ce86456103c
SajidMarakkar <72130333+saajid-marakkar@users.noreply.github.com>
2022-05-01 21:05:15 +0530

    added google analytics and upgrade to next 11

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,23 @@ import "/public/assets/css/animation.css"
 import "/public/assets/css/header.css"
 import "/public/assets/css/slider.css"
 import "/public/assets/css/contactme.css"
-function MyApp({ Component, pageProps }) {
+import Script from "next/script";
 
+function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="lazyOnload"
+        src='https://www.googletagmanager.com/gtag/js?id=G-3P96W5HF7H' />
+      <Script strategy="lazyOnload">
+        {
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+      
+          gtag('config', 'G-3P96W5HF7H');
+          `
+        }
+      </Script >
       <Apphead />
       <Component {...pageProps} />
       <Footer />
