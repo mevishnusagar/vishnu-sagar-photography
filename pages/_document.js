20d804670699a558ae693b224df6c7aff3595875
SajidMarakkar <72130333+saajid-marakkar@users.noreply.github.com>
2022-05-02 02:07:25 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,8 +12,20 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" ></link>
-          <link href="https://fonts.cdnfonts.com/css/american-captain?styles=15583" rel="stylesheet"/>
+          <link href="https://fonts.cdnfonts.com/css/american-captain?styles=15583" rel="stylesheet" />
           <script type="text/javascript" src="/assets/js/colcade2.js"></script>
+          <Script strategy="lazyOnload"
+            src='https://www.googletagmanager.com/gtag/js?id=G-3P96W5HF7H' />
+          <Script strategy="lazyOnload">
+            {
+              `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+          
+              gtag('config', 'G-3P96W5HF7H');
+              `
+            }
+          </Script >
           {/* <title>Vishnu Sagar Photography</title> */}
           <meta charSet="utf-8" />
           {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
@@ -20,12 +33,12 @@ class MyDocument extends Document {
           <meta property="og:type" content="Vishnu Sagar photography" />
           <meta property="og:url" content="https://www.vishnusagarphotography.com/" />
           <meta property="og:image:url" content="/assets/images/black-height-updated.png" />
-          <meta property="og:description" content="Vishnu Sagar is a photographer based in canada"/>
+          <meta property="og:description" content="Vishnu Sagar is a photographer based in canada" />
           <meta property="og:image" content="/assets/images/black-height-updated.png" />
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="200" />
           <meta property="og:image:height" content="200" />
-          <meta property="og:site_name" content="Vishnu Sagar photography"/>
+          <meta property="og:site_name" content="Vishnu Sagar photography" />
         </Head>
         <body>
           <Main />
